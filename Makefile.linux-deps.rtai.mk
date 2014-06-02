bf84abf0820524f0cbedbcf6a12d909904dab4bc
John Morris <john@zultron.com>
2014-06-02 07:48:37 +0200

    Renmuber RTAI rules to 11

--- Makefile.linux-deps.rtai.mk
+++ Makefile.linux-deps.rtai.mk
@@ -1,5 +1,5 @@
 ###################################################
-# 3.1. RTAI build rules
+# 11. RTAI build rules
 #
 # Included by Makefile.main.linux.mk
 
@@ -12,10 +12,10 @@
 #     rtai
 
 
-# 3.1.1. clone & update the rtai submodule
-stamps/3.1.1.rtai-source-checkout: \
+# 11.1. clone & update the rtai submodule
+stamps/11.1.rtai-source-checkout: \
 		stamps/0.1.base-builddeps
-	@echo "===== 3.1.1. All variants:  Checking out RTAI git repo ====="
+	@echo "===== 11.1. All variants:  Checking out RTAI git repo ====="
 	$(REASON)
 	mkdir -p git/rtai
 #	# be sure the submodule has been checked out
@@ -23,19 +23,19 @@ stamps/3.1.1.rtai-source-checkout: \
            git submodule update --init -- git/rtai
 	git submodule update git/rtai
 	touch $@
-.PRECIOUS: stamps/3.1.1.rtai-source-checkout
+.PRECIOUS: stamps/11.1.rtai-source-checkout
 
 clean-rtai-source-checkout: \
 		clean-rtai-source-package
 	@echo "cleaning up RTAI git submodule directory"
 	rm -rf git/rtai; mkdir -p git/rtai
-	rm -f stamps/3.1.1.rtai-source-checkout
+	rm -f stamps/11.1.rtai-source-checkout
 SQUEAKY_CLEAN += clean-rtai-source-checkout
 
-# 3.1.2. clone & update the rtai-deb submodule
-stamps/3.1.2.rtai-deb-source-checkout: \
+# 11.2. clone & update the rtai-deb submodule
+stamps/11.2.rtai-deb-source-checkout: \
 		stamps/0.1.base-builddeps
-	@echo "===== 3.1.2. All variants: " \
+	@echo "===== 11.2. All variants: " \
 	    "Checking out RTAI Debian git repo ====="
 	$(REASON)
 	mkdir -p git/rtai-deb
@@ -44,19 +44,19 @@ stamps/3.1.2.rtai-deb-source-checkout: \
            git submodule update --init -- git/rtai-deb
 	git submodule update git/rtai-deb
 	touch $@
-.PRECIOUS: stamps/3.1.2.rtai-deb-source-checkout
+.PRECIOUS: stamps/11.2.rtai-deb-source-checkout
 
 clean-rtai-deb-source-checkout: \
 		clean-rtai-source-package
 	@echo "cleaning up RTAI Debian git submodule directory"
 	rm -rf git/rtai-deb; mkdir -p git/rtai-deb
-	rm -f stamps/3.1.2.rtai-deb-source-checkout
+	rm -f stamps/11.2.rtai-deb-source-checkout
 SQUEAKY_CLEAN += clean-rtai-deb-source-checkout
 
-# 3.1.3. Build RTAI orig source tarball
-stamps/3.1.3.rtai-source-tarball: \
-		stamps/3.1.1.rtai-source-checkout
-	@echo "===== 3.1.3. All variants:  Building RTAI source tarball ====="
+# 11.3. Build RTAI orig source tarball
+stamps/11.3.rtai-source-tarball: \
+		stamps/11.1.rtai-source-checkout
+	@echo "===== 11.3. All variants:  Building RTAI source tarball ====="
 	$(REASON)
 	mkdir -p src/rtai
 	rm -f src/rtai/rtai_*.orig.tar.gz
@@ -65,21 +65,21 @@ stamps/3.1.3.rtai-source-tarball: \
 	git --git-dir="git/rtai/.git" archive HEAD | \
 	    gzip > src/rtai/rtai_$${RTAI_VER}.orig.tar.gz
 	touch $@
-.PRECIOUS: stamps/3.1.3.rtai-source-tarball
+.PRECIOUS: stamps/11.3.rtai-source-tarball
 
 clean-rtai-source-tarball: \
 		clean-rtai-source-package
 	@echo "cleaning up unpacked rtai source"
 	rm -f src/rtai/rtai_*.dsc
 	rm -f src/rtai/rtai_*.tar.gz
-	rm -f stamps/3.1.3.rtai-source-tarball
+	rm -f stamps/11.3.rtai-source-tarball
 CLEAN_TARGETS += clean-rtai-source-tarball
 
-# 3.1.4. Build RTAI source package
-stamps/3.1.4.rtai-source-package: \
-		stamps/3.1.2.rtai-deb-source-checkout \
-		stamps/3.1.3.rtai-source-tarball
-	@echo "===== 3.1.4. All variants:  Build RTAI source package ====="
+# 11.4. Build RTAI source package
+stamps/11.4.rtai-source-package: \
+		stamps/11.2.rtai-deb-source-checkout \
+		stamps/11.3.rtai-source-tarball
+	@echo "===== 11.4. All variants:  Build RTAI source package ====="
 	$(REASON)
 	rm -rf src/rtai/build; mkdir -p src/rtai/build
 	rm -f src/rtai/rtai_*.dsc
@@ -89,19 +89,19 @@ stamps/3.1.4.rtai-source-package: \
 	    tar xCf src/rtai/build -
 	cd src/rtai && dpkg-source -i -I -b build
 	touch $@
-.PRECIOUS: stamps/3.1.4.rtai-source-package
+.PRECIOUS: stamps/11.4.rtai-source-package
 
 clean-rtai-source-package: \
 		$(call CA_EXPAND,%/clean-rtai-build)
 	rm -rf src/rtai/build
-	rm -f stamps/3.1.4.rtai-source-package
+	rm -f stamps/11.4.rtai-source-package
 CLEAN_TARGETS += clean-rtai-source-tarball
 
-# 3.1.5. Build the RTAI binary packages
-%/.stamp.3.1.5.rtai-build: \
+# 11.5. Build the RTAI binary packages
+%/.stamp.11.5.rtai-build: \
 		%/.stamp.2.1.chroot-build \
-		stamps/3.1.4.rtai-source-package
-	@echo "===== 3.1.5. $(CA):  Building RTAI binary packages ====="
+		stamps/11.4.rtai-source-package
+	@echo "===== 11.5. $(CA):  Building RTAI binary packages ====="
 	$(REASON)
 #	# ARM arch is broken
 #	# jessie is broken (no libcomedi)
@@ -110,7 +110,7 @@ CLEAN_TARGETS += clean-rtai-source-tarball
 		--build $(PBUILD_ARGS) \
 	        src/rtai/rtai_*.dsc
 	touch $@
-.PRECIOUS: %/.stamp.3.1.5.rtai-build
+.PRECIOUS: %/.stamp.11.5.rtai-build
 
 %/clean-rtai-build:
 	@echo "cleaning up $* rtai binary-build"
@@ -130,6 +130,6 @@ ARCH_CLEAN_TARGETS += rtai-build
 
 # Hook into rest of build
 ifneq ($(filter rtai,$(FEATURESETS_ENABLED)),)
-PPA_INTERMEDIATE_DEPS += %/.stamp.3.1.5.rtai-build
+PPA_INTERMEDIATE_DEPS += %/.stamp.11.5.rtai-build
 endif
 
