fc0df03c8ffb2a1e44d547394ba553de19712780
John Morris <john@zultron.com>
2014-06-02 07:45:08 +0200

    Renumber linux rules to 15

--- Makefile.20.linux.mk
+++ Makefile.20.linux.mk
@@ -1,5 +1,5 @@
 ###################################################
-# 20. Linux kernel build rules
+# 15. Linux kernel build rules
 
 ###################################################
 # Variables that may change
@@ -33,86 +33,86 @@ FEATURESETS_DISABLED = $(filter-out $(FEATURESETS_ENABLED),$(FEATURESETS))
 
 
 ###################################################
-# 20.1. Check out git submodule
-stamps/20.1.linux-kernel-package-checkout: \
+# 15.1. Check out git submodule
+stamps/15.1.linux-kernel-package-checkout: \
 		stamps/0.1.base-builddeps
-	@echo "===== 20.1. All variants: "\
+	@echo "===== 15.1. All variants: "\
 	    "Checking out kernel Debian git repo ====="
 	$(REASON)
 #	# be sure the submodule has been checked out
 	git submodule update --recursive --init git/kernel-rt-deb2
 	touch $@
 
-stamps/20.1.linux-kernel-package-checkout-clean: \
+stamps/15.1.linux-kernel-package-checkout-clean: \
 		$(call CA_EXPAND,\
-			stamps/20.3.%.linux-kernel-deps-update-chroot-clean)
-	@echo "20.1. All:  Clean linux kernel packaging git submodule stamp"
-	rm -f stamps/20.1.linux-kernel-package-checkout
+			stamps/15.3.%.linux-kernel-deps-update-chroot-clean)
+	@echo "15.1. All:  Clean linux kernel packaging git submodule stamp"
+	rm -f stamps/15.1.linux-kernel-package-checkout
 
-stamps/20.1.linux-kernel-package-checkout-squeaky: \
-		stamps/20.1.linux-kernel-package-checkout-clean
-	@echo "20.1. All:  Clean linux kernel packaging git submodule"
+stamps/15.1.linux-kernel-package-checkout-squeaky: \
+		stamps/15.1.linux-kernel-package-checkout-clean
+	@echo "15.1. All:  Clean linux kernel packaging git submodule"
 	rm -rf git/kernel-rt-deb2; mkdir -p git/kernel-rt-deb2
-LINUX_SQUEAKY_ALL += stamps/20.1.linux-kernel-package-checkout-squeaky
+LINUX_SQUEAKY_ALL += stamps/15.1.linux-kernel-package-checkout-squeaky
 
 
 ###################################################
-# 20.2. Download linux tarball
-stamps/20.2.linux-kernel-tarball-downloaded: \
+# 15.2. Download linux tarball
+stamps/15.2.linux-kernel-tarball-downloaded: \
 		stamps/0.1.base-builddeps
-	@echo "===== 20.2. All variants: " \
+	@echo "===== 15.2. All variants: " \
 	    "Downloading vanilla Linux tarball ====="
 	$(REASON)
 	rm -f dist/$(LINUX_TARBALL)
 	wget $(LINUX_URL)/$(LINUX_TARBALL) -O dist/$(LINUX_TARBALL)
 	touch $@
 # This target is needed by linux-tools
-LINUX_TARBALL_TARGET := stamps/20.2.linux-kernel-tarball-downloaded
+LINUX_TARBALL_TARGET := stamps/15.2.linux-kernel-tarball-downloaded
 
-stamps/20.2.linux-kernel-tarball-downloaded-clean: \
+stamps/15.2.linux-kernel-tarball-downloaded-clean: \
 		$(call CA_EXPAND,\
-			stamps/20.3.%.linux-kernel-deps-update-chroot-clean)
-	@echo "20.2. All:  Clean up linux kernel tarball"
+			stamps/15.3.%.linux-kernel-deps-update-chroot-clean)
+	@echo "15.2. All:  Clean up linux kernel tarball"
 	rm -f dist/$(LINUX_TARBALL)
-	rm -f stamps/20.2.linux-kernel-tarball-downloaded
-LINUX_SQUEAKY_ALL += stamps/20.2.linux-kernel-tarball-downloaded
+	rm -f stamps/15.2.linux-kernel-tarball-downloaded
+LINUX_SQUEAKY_ALL += stamps/15.2.linux-kernel-tarball-downloaded
 
 
 ###################################################
-# 20.3. Update chroot with dependent packages
+# 15.3. Update chroot with dependent packages
 
 # Any indep targets should be added to $(LINUX_KERNEL_DEPS_INDEP), and
 # arch or all targets should be added to $(LINUX_KERNEL_DEPS)
-$(call CA_TO_C_DEPS,stamps/20.3.%.linux-kernel-deps-update-chroot,\
+$(call CA_TO_C_DEPS,stamps/15.3.%.linux-kernel-deps-update-chroot,\
 	$(LINUX_KERNEL_DEPS_INDEP))
-$(call CA_EXPAND,stamps/20.3.%.linux-kernel-deps-update-chroot): \
-stamps/20.3.%.linux-kernel-deps-update-chroot: \
-		stamps/20.1.linux-kernel-package-checkout \
-		stamps/20.2.linux-kernel-tarball-downloaded \
+$(call CA_EXPAND,stamps/15.3.%.linux-kernel-deps-update-chroot): \
+stamps/15.3.%.linux-kernel-deps-update-chroot: \
+		stamps/15.1.linux-kernel-package-checkout \
+		stamps/15.2.linux-kernel-tarball-downloaded \
 		$(LINUX_KERNEL_DEPS)
-	$(call UPDATE_CHROOT,20.3)
-.PRECIOUS: $(call CA_EXPAND,stamps/20.3.%.linux-kernel-deps-update-chroot)
+	$(call UPDATE_CHROOT,15.3)
+.PRECIOUS: $(call CA_EXPAND,stamps/15.3.%.linux-kernel-deps-update-chroot)
 
-$(call CA_EXPAND,stamps/20.3.%.linux-kernel-deps-update-chroot-clean): \
-stamps/20.3.%.linux-kernel-deps-update-chroot-clean:
-	@echo "20.3. $(CA):  Clean linux kernel chroot deps update stamp"
-	rm -f stamps/20.3.$(CA).linux-kernel-deps-update-chroot
-$(call CA_TO_C_DEPS,stamps/20.3.%.linux-kernel-deps-update-chroot-clean,\
-	stamps/20.5.%.linux-kernel-source-package-clean)
+$(call CA_EXPAND,stamps/15.3.%.linux-kernel-deps-update-chroot-clean): \
+stamps/15.3.%.linux-kernel-deps-update-chroot-clean:
+	@echo "15.3. $(CA):  Clean linux kernel chroot deps update stamp"
+	rm -f stamps/15.3.$(CA).linux-kernel-deps-update-chroot
+$(call CA_TO_C_DEPS,stamps/15.3.%.linux-kernel-deps-update-chroot-clean,\
+	stamps/15.5.%.linux-kernel-source-package-clean)
 
 # Cleaning this cleans up all (non-squeaky) linux arch and indep artifacts
-LINUX_CLEAN_ARCH := stamps/20.3.%.linux-kernel-deps-update-chroot-clean
+LINUX_CLEAN_ARCH := stamps/15.3.%.linux-kernel-deps-update-chroot-clean
 
 
 ###################################################
-# 20.4. Unpack and configure Linux package source tree
+# 15.4. Unpack and configure Linux package source tree
 
 # This has to be done in a chroot with the featureset packages
-stamps/20.4.linux-kernel-package-configured: CODENAME = $(A_CODENAME)
-stamps/20.4.linux-kernel-package-configured: ARCH = $(AN_ARCH)
-stamps/20.4.linux-kernel-package-configured: \
-		stamps/20.3.$(A_CHROOT).linux-kernel-deps-update-chroot
-	@echo "===== 20.4. All:  Unpacking and configuring" \
+stamps/15.4.linux-kernel-package-configured: CODENAME = $(A_CODENAME)
+stamps/15.4.linux-kernel-package-configured: ARCH = $(AN_ARCH)
+stamps/15.4.linux-kernel-package-configured: \
+		stamps/15.3.$(A_CHROOT).linux-kernel-deps-update-chroot
+	@echo "===== 15.4. All:  Unpacking and configuring" \
 	    " Linux source package ====="
 	$(REASON)
 #	# Starting clean, copy debian packaging and hardlink source tarball
@@ -138,24 +138,24 @@ stamps/20.4.linux-kernel-package-configured: \
 	cd src/linux/build && debian/rules orig
 	cd src/linux/build && debian/rules clean
 	touch $@
-.PRECIOUS: stamps/20.4.linux-kernel-package-configured
+.PRECIOUS: stamps/15.4.linux-kernel-package-configured
 
-stamps/20.4.linux-kernel-package-configured-clean: \
-		$(call C_EXPAND,stamps/20.5.%.linux-kernel-source-package-clean)
-	@echo "20.4.  All: Clean configured linux kernel source directory"
+stamps/15.4.linux-kernel-package-configured-clean: \
+		$(call C_EXPAND,stamps/15.5.%.linux-kernel-source-package-clean)
+	@echo "15.4.  All: Clean configured linux kernel source directory"
 	rm -rf src/linux
 	rm -f pkgs/$(LINUX_TARBALL_DEBIAN_ORIG)
-	rm -f stamps/20.4.linux-kernel-package-configured
-LINUX_CLEAN_ALL += stamps/20.4.linux-kernel-package-configured-clean
+	rm -f stamps/15.4.linux-kernel-package-configured
+LINUX_CLEAN_ALL += stamps/15.4.linux-kernel-package-configured-clean
 
 
 ###################################################
-# 20.5. Build Linux kernel source package for each distro
-$(call C_EXPAND,stamps/20.5.%.linux-kernel-source-package): \
-stamps/20.5.%.linux-kernel-source-package: \
-		stamps/20.1.linux-kernel-package-checkout \
-		stamps/20.4.linux-kernel-package-configured
-	@echo "===== 20.5. $(CODENAME)-all: " \
+# 15.5. Build Linux kernel source package for each distro
+$(call C_EXPAND,stamps/15.5.%.linux-kernel-source-package): \
+stamps/15.5.%.linux-kernel-source-package: \
+		stamps/15.1.linux-kernel-package-checkout \
+		stamps/15.4.linux-kernel-package-configured
+	@echo "===== 15.5. $(CODENAME)-all: " \
 	    "Building Linux source package ====="
 	$(REASON)
 #	# Restore original changelog
@@ -169,28 +169,28 @@ stamps/20.5.%.linux-kernel-source-package: \
 	mv src/linux/linux_$(LINUX_PKG_VERSION).debian.tar.xz \
 	    src/linux/linux_$(LINUX_PKG_VERSION).dsc pkgs
 	touch $@
-.PRECIOUS: $(call C_EXPAND,stamps/20.5.%.linux-kernel-source-package)
+.PRECIOUS: $(call C_EXPAND,stamps/15.5.%.linux-kernel-source-package)
 
-$(call C_EXPAND,stamps/20.5.%.linux-kernel-source-package-clean): \
-stamps/20.5.%.linux-kernel-source-package-clean:
-	@echo "20.5.  $(CODENAME):  Clean linux kernel source build"
+$(call C_EXPAND,stamps/15.5.%.linux-kernel-source-package-clean): \
+stamps/15.5.%.linux-kernel-source-package-clean:
+	@echo "15.5.  $(CODENAME):  Clean linux kernel source build"
 	rm -f pkgs/linux_$(LINUX_PKG_VERSION).debian.tar.xz
 	rm -f pkgs/linux_$(LINUX_PKG_VERSION).dsc
-	rm -f stamps/20.5.linux-kernel-source-package
-$(call C_TO_CA_DEPS,stamps/20.5.%.linux-kernel-source-package-clean,\
-	stamps/20.6.%.linux-kernel-build-clean)
+	rm -f stamps/15.5.linux-kernel-source-package
+$(call C_TO_CA_DEPS,stamps/15.5.%.linux-kernel-source-package-clean,\
+	stamps/15.6.%.linux-kernel-build-clean)
 
 ###################################################
-# 20.6. Build kernel packages for each distro/arch
+# 15.6. Build kernel packages for each distro/arch
 #
 # Use the PPA with featureset devel packages
-$(call CA_TO_C_DEPS,stamps/20.6.%.linux-kernel-build,\
-	stamps/20.5.%.linux-kernel-source-package)
+$(call CA_TO_C_DEPS,stamps/15.6.%.linux-kernel-build,\
+	stamps/15.5.%.linux-kernel-source-package)
 
-$(call CA_EXPAND,stamps/20.6.%.linux-kernel-build): \
-stamps/20.6.%.linux-kernel-build: \
-		stamps/20.3.%.linux-kernel-deps-update-chroot
-	@echo "===== 20.6. $(CA):  Building Linux binary package ====="
+$(call CA_EXPAND,stamps/15.6.%.linux-kernel-build): \
+stamps/15.6.%.linux-kernel-build: \
+		stamps/15.3.%.linux-kernel-deps-update-chroot
+	@echo "===== 15.6. $(CA):  Building Linux binary package ====="
 	$(REASON)
 	$(SUDO) INTERMEDIATE_REPO=ppa \
 	    $(PBUILD) --build \
@@ -198,46 +198,46 @@ stamps/20.6.%.linux-kernel-build: \
 	        pkgs/linux_$(LINUX_PKG_VERSION).dsc || \
 	    (rm -f $@ && exit 1)
 	touch $@
-.PRECIOUS: $(call CA_EXPAND,stamps/20.6.%.linux-kernel-build)
-LINUX_ARTIFACTS_ARCH += stamps/20.6.%.linux-kernel-build
+.PRECIOUS: $(call CA_EXPAND,stamps/15.6.%.linux-kernel-build)
+LINUX_ARTIFACTS_ARCH += stamps/15.6.%.linux-kernel-build
 
-$(call CA_EXPAND,stamps/20.6.%.linux-kernel-build-clean): \
-stamps/20.6.%.linux-kernel-build-clean:
-	@echo "20.6.  $(CA):  Clean linux kernel binary builds"
+$(call CA_EXPAND,stamps/15.6.%.linux-kernel-build-clean): \
+stamps/15.6.%.linux-kernel-build-clean:
+	@echo "15.6.  $(CA):  Clean linux kernel binary builds"
 	rm -f $(wildcard pkgs/linux-headers-*_$(LINUX_PKG_VERSION)_$(ARCH).deb)
 	rm -f $(wildcard pkgs/linux-image-*_$(LINUX_PKG_VERSION)_$(ARCH).deb)
 	rm -f pkgs/linux_$(LINUX_PKG_VERSION)-$(ARCH).build
 	rm -f pkgs/linux_$(LINUX_PKG_VERSION)_$(ARCH).changes
-	rm -f stamps/20.6.$*.linux-kernel-build
-$(call CA_TO_C_DEPS,stamps/20.6.%.linux-kernel-build-clean,\
-	stamps/20.7.%.linux-kernel-ppa-clean)
+	rm -f stamps/15.6.$*.linux-kernel-build
+$(call CA_TO_C_DEPS,stamps/15.6.%.linux-kernel-build-clean,\
+	stamps/15.7.%.linux-kernel-ppa-clean)
 
 ###################################################
-# 20.7. Add kernel packages to the PPA for each distro
+# 15.7. Add kernel packages to the PPA for each distro
 
 # e.g.:
 # linux-headers-3.8-1mk-common-xenomai.x86_3.8.13-1mk~wheezy1_i386.deb
 # linux-headers-3.8-1mk-xenomai.x86-686-pae_3.8.13-1mk~wheezy1_i386.deb
 # linux-image-3.8-1mk-xenomai.x86-686-pae_3.8.13-1mk~wheezy1_i386.deb
-$(call C_TO_CA_DEPS,stamps/20.7.%.linux-kernel-ppa,\
-	stamps/20.6.%.linux-kernel-build)
-$(call C_EXPAND,stamps/20.7.%.linux-kernel-ppa): \
-stamps/20.7.%.linux-kernel-ppa: \
-		stamps/20.5.%.linux-kernel-source-package \
+$(call C_TO_CA_DEPS,stamps/15.7.%.linux-kernel-ppa,\
+	stamps/15.6.%.linux-kernel-build)
+$(call C_EXPAND,stamps/15.7.%.linux-kernel-ppa): \
+stamps/15.7.%.linux-kernel-ppa: \
+		stamps/15.5.%.linux-kernel-source-package \
 		stamps/0.3.all.ppa-init
-	$(call BUILD_PPA,20.7,linux,\
+	$(call BUILD_PPA,15.7,linux,\
 	    pkgs/linux_$(LINUX_PKG_VERSION).dsc,\
 	    $(foreach a,$(call CODENAME_ARCHES,$(CODENAME)),$(wildcard\
 		pkgs/linux-headers-*_$(LINUX_PKG_VERSION)_$(a).deb \
 		pkgs/linux-image-*_$(LINUX_PKG_VERSION)_$(a).deb)))
 
 # This is the final result of the linux kernel build
-LINUX_INDEP := stamps/20.7.%.linux-kernel-ppa
+LINUX_INDEP := stamps/15.7.%.linux-kernel-ppa
 
-$(call C_EXPAND,stamps/20.7.%.linux-kernel-ppa-clean): \
-stamps/20.7.%.linux-kernel-ppa-clean:
-	@echo "20.7.  $(CODENAME):  Clean linux kernel PPA stamp"
-	rm -f stamps/20.7.%.linux-kernel-ppa-clean
+$(call C_EXPAND,stamps/15.7.%.linux-kernel-ppa-clean): \
+stamps/15.7.%.linux-kernel-ppa-clean:
+	@echo "15.7.  $(CODENAME):  Clean linux kernel PPA stamp"
+	rm -f stamps/15.7.%.linux-kernel-ppa-clean
 
 
 # Hook kernel build into final build
