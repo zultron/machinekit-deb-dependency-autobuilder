274ed354d01137d565c49120d5fdbb586e5f08b5
John Morris <john@zultron.com>
2014-07-14 13:13:48 -0500

    Updates to linux submodule; bump pkg release to `-5da`
    - Add `Provides: linux-image-<xenomai|rtai>` to pkgs
    - Remove `Provides: linux-image-<featureset>` hard-coded hack
    - Add `Provides: rtai-modules-...` to RTAI kernel package
      - For compatibility with LinuxCNC project kernel pkgs
    - Allow templating package relations
      - e.g. `Provides: rtai-modules-%(abiname)s-rtai.x86-%(flavour)s`
    - New `bin/gencontrol.py --dump-config` command line argument
      - Handy for debugging

--- Makefiles/Makefile.15.linux.mk
+++ Makefiles/Makefile.15.linux.mk
@@ -37,7 +37,7 @@
 # Variables that may change
 
 # Pkg release number; bump this for every new package
-LINUX_PKG_RELEASE = 2da
+LINUX_PKG_RELEASE = 5da
 # Linux vanilla tarball version
 LINUX_VERSION = 3.8.13
 # An ABI name; must match the `abiname` defined in config/defines
