572b535af77d2e78deddc52a14062d61d9372029
John Morris <john@zultron.com>
2014-07-22 01:36:54 -0500

    Initial commit of dovetail-automata-keyring

---
+++ Makefiles/Makefile.90.dovetail-automata-keyring.mk
@@ -0,0 +1,42 @@
+###################################################
+# 90. dovetail-automata-keyring build rules
+
+###################################################
+# Variables that may change
+
+# Dovetail-Automata-Keyring package
+DOVETAIL_AUTOMATA_KEYRING_PKG_RELEASE = 1
+DOVETAIL_AUTOMATA_KEYRING_VERSION = 0.1
+
+
+###################################################
+# Variables that should not change much
+# (or auto-generated)
+
+# Source name
+DOVETAIL_AUTOMATA_KEYRING_SOURCE_NAME := dovetail-automata-keyring
+
+# Index
+DOVETAIL_AUTOMATA_KEYRING_INDEX := 90
+
+# Arch-indep pkg
+DOVETAIL_AUTOMATA_KEYRING_ARCH := INDEP
+
+# Submodule name
+DOVETAIL_AUTOMATA_KEYRING_SUBMODULE := git/dovetail-automata-keyring-deb
+
+# Packages; will be suffixed by _<pkg_version>_<arch>.deb
+DOVETAIL_AUTOMATA_KEYRING_PKGS_ALL := dovetail-automata-keyring
+DOVETAIL_AUTOMATA_KEYRING_PKGS_ARCH := 
+
+# Misc paths, filenames, executables
+DOVETAIL_AUTOMATA_KEYRING_COMPRESSION = gz
+# No tarball
+DOVETAIL_AUTOMATA_KEYRING_TARBALL := 
+DOVETAIL_AUTOMATA_KEYRING_URL = 
+
+
+###################################################
+# Do the standard build for this package
+$(eval $(call TARGET_VARS,DOVETAIL_AUTOMATA_KEYRING))
+$(eval $(call DEBUG_BUILD,DOVETAIL_AUTOMATA_KEYRING))
